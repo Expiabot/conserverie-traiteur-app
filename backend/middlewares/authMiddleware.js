7458f9300b9ffd7bb38be0bad5844ec6790c5d70
Expiabot <stbnmarteil@gmail.com>
2025-03-04 18:03:03 +0100

    Ajout du middleware d'authentification

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,47 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/user');
+
+// Middleware pour protéger les routes
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Récupérer le token du header
+      token = req.headers.authorization.split(' ')[1];
+
+      // Vérifier le token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Obtenir l'utilisateur à partir du token
+      req.user = await User.findById(decoded.id).select('-password');
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Non autorisé, token invalide');
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Non autorisé, pas de token');
+  }
+});
+
+// Middleware pour vérifier si l'utilisateur est admin
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Non autorisé, accès administrateur requis');
+  }
+};
+
+module.exports = { protect, admin };
